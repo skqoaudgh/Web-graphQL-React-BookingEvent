52f026d4a392634681638f6078c2ca0def298d69
MyunghoBae <bmh961@naver.com>
2019-05-02 13:25:09 +0900

    [Add] GraphQL Schema와 Resolver 프로토타입 작성

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 const express = require('express');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req,res, next) => {
-    res.send('Hello World!');
-});
+app.use('/api', graphqlHttp({
+    // define real endpoint
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: { // using example data now
+        events: () => {
+            return ['Romantic Cooking', 'Sailing', 'Something'];
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
 
 app.listen(8000);
