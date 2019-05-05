86892751ef4d7b48322694351ea9ef59a8b9f158
myungho <skqoaudgh2@hanmail.net>
2019-05-05 22:20:33 +0900

    [Update] Resolver 파일을 여러 파일로 분리

---
+++ graphql/resolver/events.js
@@ -0,0 +1,43 @@
+const Event = require('../../models/event');
+const { transformEvent } = require('./merge');
+
+module.exports = {
+    events: async () => {
+        try {
+            const events = await Event.find();
+            return events.map(event => {
+                return transformEvent(event);
+            });
+        }
+        catch(err) {
+            throw err;
+        }
+    },
+
+    createEvent: async args => {
+        const event = new Event({
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: args.eventInput.price,
+            date: new Date(args.eventInput.date),
+            creator: '5cced48bafcbf74748341d3f'
+        });
+        let createdEvent;
+        try {
+            const result = await event.save()
+            createdEvent = transformEvent(result);
+            const creator = await User.findById('5cced48bafcbf74748341d3f');
+
+            if(!creator) {
+                throw new Error('User not found');
+            }
+            creator.createdEvents.push(event);
+            await creator.save();
+
+            return createdEvent;
+        }
+        catch(err) {
+            throw err;
+        }
+    }
+}
