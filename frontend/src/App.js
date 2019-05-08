424c7fcf9a53423fb87813bb43ac49b530f18513
myungho <skqoaudgh2@hanmail.net>
2019-05-08 22:47:05 +0900

    [Add] Context API를 이용하여 Token정보를 각 컴포넌트가 활용할 수 있도록 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 import AuthPage from './pages/Auth';
 import EventsPage from './pages/Events';
 import BookingsPage from './pages/Bookings';
 import MainNavigation from './components/Navigation/MainNavigation';
+import AuthContext from './context/auth-context';
 
 import './App.css';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <MainNavigation />
-      <main className="main-content">
-        <Switch>
-          <Redirect path="/" to="/auth" exact/>
-          <Route path="/auth" component={AuthPage}/>
-          <Route path="/events" component={EventsPage}/>
-          <Route path="/bookings" component={BookingsPage}/>
-        </Switch>
-      </main>
-    </BrowserRouter>
+class App extends Component {
+  state = {
+    token: null,
+    userId: null
+  }
+
+  login = (token, userId, tokenExpiration) => {
+    this.setState({token: token, userId: userId});
+  };
+  
+  logout = () => {
+    this.setState({token: null, userId: null});
+  };
+
+  render() {
+    return (
+      <BrowserRouter>
+      <AuthContext.Provider
+          value={{
+            token: this.state.token, 
+            userId: this.state.userId, 
+            login: this.login,
+            logout: this.logout
+          }}
+        >
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              {!this.state.token && <Redirect path="/" to="/auth" exact/>}
+              {this.state.token && <Redirect path="/" to="/events" exact/>}
+              {this.state.token && <Redirect path="/auth" to="/events" exact/>}
+              {!this.state.token && <Route path="/auth" component={AuthPage}/>}
+              <Route path="/events" component={EventsPage}/>
+              {this.state.token && <Route path="/bookings" component={BookingsPage}/>}
+            </Switch>
+          </main>
+        </AuthContext.Provider>
+      </BrowserRouter>
     );
+  }
 }
 
 export default App;
--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -1,27 +1,39 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import AuthContext from '../../context/auth-context';
+
 import './MainNavigation.css';
 
 const mainNavigation = props => (
-    <header className="main-navigation">
-        <div className="main-navigation_logo">
-            <h1>The NavBar</h1>
-        </div>
-        <div className="main-navigation_item">
-            <ul>
-                <li>
-                    <NavLink to="/auth">Authenticate</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/events">Events</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/bookings">Bookings</NavLink>
-                </li>
-            </ul>
-        </div>
-    </header>
+    <AuthContext.Consumer>
+        {(context) => {
+            return (
+                <header className="main-navigation">
+                    <div className="main-navigation_logo">
+                        <h1>The NavBar</h1>
+                    </div>
+                    <div className="main-navigation_item">
+                        <ul>
+                            {!context.token && (
+                                <li>
+                                    <NavLink to="/auth">Authenticate</NavLink>
+                                </li> 
+                            )}
+                            <li>
+                                <NavLink to="/events">Events</NavLink>
+                            </li>
+                            {context.token && (
+                                <li>
+                                    <NavLink to="/bookings">Bookings</NavLink>
+                                </li>
+                            )}
+                        </ul>
+                    </div>
+                </header>  
+            );
+        }}
+    </AuthContext.Consumer>
 );
 
 export default mainNavigation;
