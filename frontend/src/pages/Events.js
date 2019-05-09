949febbf34f84e5bc714e9956994ef46cc56c6ea
myungho <skqoaudgh2@hanmail.net>
2019-05-09 20:12:58 +0900

    [Add] Event 리스트가 표시되도록 추가

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 
 import Modal from '../components/Modal/Modal';
-import Backdrop from '../components/Backdrop/Backdrop'
+import Backdrop from '../components/Backdrop/Backdrop';
+import AuthContext from '../context/auth-context';
+
 import './Events.css';
 
 class EventsPage extends Component {
     state = {
-        creating: false
+        creating: false,
+        events: []
     };
+
+    static contextType = AuthContext;
+
+    constructor(props) {
+        super(props);
+        this.titleEl = React.createRef();
+        this.priceEl = React.createRef();
+        this.dateEl = React.createRef();
+        this.descriptionEl = React.createRef();
+    }
+
+    componentDidMount() {
+        this.fetchEvents();
+    }
 
     startCreateEventHandelr = () => {
         this.setState({creating: true});
@@ -17,21 +34,144 @@ class EventsPage extends Component {
         this.setState({creating: false});
     };
 
+    fetchEvents() {
+        const requestBody = {
+            query: `
+                query {
+                    events {
+                        _id
+                        title
+                        description
+                        date
+                        price
+                        creator {
+                            _id
+                            email
+                        }
+                    }
+                }
+            `
+        };
+
+        fetch('http://localhost:8000/api', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        }).then(res => {
+            if(res.status !== 200 && res.status !== 201) {
+                throw new Error('Failed!');
+            }
+            return res.json();
+        })
+        .then(resData => {
+            const events = resData.data.events;
+            this.setState({events: events});
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }
+
     modalConfirmHandler = () => {
         this.setState({creating: false});
+        const title = this.titleEl.current.value;
+        const price = +this.priceEl.current.value;
+        const date = this.dateEl.current.value;
+        const description = this.descriptionEl.current.value;
+
+        if(title.trim().length === 0 || price <= 0 || date.trim().length === 0 || description.trim().length === 0) {
+            return;
+        }
+
+        const event = {title, price, date, description};
+        console.log(event);
+
+        const requestBody = {
+            query: `
+                mutation {
+                    createEvent(eventInput: {title: "${title}", description: "${description}", price: ${price}, date: "${date}"}) {
+                        _id
+                        title
+                        description
+                        date
+                        price
+                        creator {
+                            _id
+                            email
+                        }
+                    }
+                }
+            `
+        };
+    
+        const token = this.context.token;
+
+        fetch('http://localhost:8000/api', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            }
+        }).then(res => {
+            if(res.status !== 200 && res.status !== 201) {
+                throw new Error('Failed!');
+            }
+            return res.json();
+        })
+        .then(resData => {
+            this.fetchEvents();
+        })
+        .catch(err => {
+            console.log(err);
+        });
     };
 
     render() {
+        const eventList = this.state.events.map(event => {
+            return (
+                <li key={event._id} className="events__list-item">{event.title}</li>
+            )
+        });
+
         return (
             <React.Fragment>
                 {this.state.creating && <Backdrop />}
-                {this.state.creating && <Modal title="Add Event" canCancel canConfirm onCancel={this.modalCancelHandler} onConfirm={this.modalConfirmHandler}>
-                    <p>Modal Text</p>
+                {this.state.creating && <Modal title="Add Event" 
+                    canCancel 
+                    canConfirm 
+                    onCancel={this.modalCancelHandler} 
+                    onConfirm={this.modalConfirmHandler}
+                >
+                    <form>
+                        <div className="form-control">
+                            <label htmlFor="title">Title</label>
+                            <input type="text" id="title" ref={this.titleEl}></input>
+                        </div>
+                        <div className="form-control">
+                            <label htmlFor="price">Price</label>
+                            <input type="number" id="price" ref={this.priceEl}></input>
+                        </div>
+                        <div className="form-control">
+                            <label htmlFor="date">Date</label>
+                            <input type="datetime-local" id="date" ref={this.dateEl}></input>
+                        </div>
+                        <div className="form-control">
+                            <label htmlFor="description">Description</label>
+                            <textarea id="description" rows="4" ref={this.descriptionEl}></textarea>
+                        </div>                                                                    
+                    </form>
                 </Modal>}
-                <div className="events-control">
+                {this.context.token && (<div className="events-control">
                     <p>Share your own Events!</p>
                     <button className="btn" onClick={this.startCreateEventHandelr}>Create Event</button>
                 </div>
+                )}
+                <ul className="events__list">
+                    {eventList}
+                </ul>
             </React.Fragment>
         );
     }
