5d7f02f9624dc614e1e7eade674638f87b715e0c
myungho <skqoaudgh2@hanmail.net>
2019-05-11 14:30:47 +0900

    [Add] GraphQL 쿼리문 동적변수 할당 기능 추가

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -101,8 +101,8 @@ class EventsPage extends Component {
 
         const requestBody = {
             query: `
-                mutation {
-                    createEvent(eventInput: {title: "${title}", description: "${description}", price: ${price}, date: "${date}"}) {
+                mutation CreateEvent($title: String!, $description: String!, $price: Int!, $date: String!) {
+                    createEvent(eventInput: {title: $title, description: $description, price: $price, date: $date}) {
                         _id
                         title
                         description
@@ -110,7 +110,13 @@ class EventsPage extends Component {
                         price
                     }
                 }
-            `
+            `,
+            variables: {
+                title: title,
+                description: description,
+                price: price,
+                date: date
+            }
         };
     
         const token = this.context.token;
@@ -162,14 +168,17 @@ class EventsPage extends Component {
         }
         const requestBody = {
             query: `
-                mutation {
-                    bookEvent(eventId: "${this.state.selectedEvent._id}") {
+                mutation BookEvent($id: ID!) {
+                    bookEvent(eventId: $id) {
                         _id
                         createdAt
                         updatedAt
                     }
                 }
-            `
+            `,
+            variables: {
+                id: this.state.selectedEvent._id
+            }
         };
 
         fetch('http://localhost:8000/api', {
