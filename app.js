dd0526c1d8507a9560ec337af92c8b95def9ba37
myungho <skqoaudgh2@hanmail.net>
2019-05-18 18:34:46 +0900

    [Add] OpenSSL을  이용한 HTTPS 서버 구축

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const graphqlHttp = require('express-graphql');
 const mongoose = require('mongoose');
+const fs = require('fs');
+const https = require('https');
+const forceSsl = require('express-force-ssl');
 
 const graphQlSchema = require('./graphql/schema/index');
 const graphQlResolvers = require('./graphql/resolver/index');
@@ -8,6 +11,12 @@ const isAuth = require('./middleware/is-auth');
 
 const app = express();
 
+const options = {
+    key : fs.readFileSync('./keys/key.pem'),
+    cert : fs.readFileSync('./keys/cert.pem'),
+    passphrase: '1234'
+};
+  
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -20,6 +29,7 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(forceSsl);
 app.use(isAuth);
 
 app.use('/api', graphqlHttp({
@@ -32,8 +42,10 @@ app.use('/api', graphqlHttp({
 mongoose.connect('mongodb+srv://' + process.env.MONGO_USER + ':' + process.env.MONGO_PASSWORD + '@node-rest-shop-zqnku.mongodb.net/' + process.env.MONGO_DB + '?retryWrites=true', 
 { useNewUrlParser: true})
 .then( () =>  {
-    app.listen(8000); 
+    app.listen(8000);
 })
 .catch(err=> {
     console.log(err);
 });
+
+https.createServer(options, app).listen(8001);
